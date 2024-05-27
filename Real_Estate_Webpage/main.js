f3ba32f29d3775eb839e35d40c116fe802207635
Subha Adak <shubhaadak0@gmail.com>
2024-05-27 16:31:43 +0530

    final build

--- Real_Estate_Webpage/main.js
+++ Real_Estate_Webpage/main.js
@@ -1,1 +1,19 @@
-import './style.css'
+import './main.css'
+
+document.addEventListener("DOMContentLoaded", function() {
+let menu = document.querySelector(".menu");
+let input_btn = document.querySelector(".input-btn");
+
+if (menu && input_btn) {
+    input_btn.addEventListener("change", function () {
+        if (input_btn.checked == true) {
+            menu.style.transform = "translateX(0%)";
+            document.body.style.overflow = "hidden";
+            
+        }else {
+            menu.style.transform = "translateX(100%)";
+            document.body.style.overflow = "auto";
+        }
+    });
+}
+});
